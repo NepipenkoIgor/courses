e70ce36f62e471e052e1e1711b53d8bd39e43668
Igor Nepipenko <igornepipenko@gmail.com>
2014-07-16 19:23:32 +0300

    gliffy 2

---
+++ public/js/controllers/courseEditionController.js
@@ -0,0 +1,178 @@
+/**
+ * Created by igor on 7/14/14.
+ */
+app.controller('courseEdition', function ($state,$stateParams, $location, $http, $scope, $q, courseEdit) {
+
+    var load = function() {
+      return  $q.all([
+            courseEdit.loadSubjects(),
+            courseEdit.loadModule()
+        ]).then(function(result) {
+            $scope.loadSubjects=result[0].data;
+            $scope.loadModule=result[1].data;
+
+        });
+    };
+    load().then(function(){
+        if($stateParams.course){
+            $scope.changeCourse($stateParams.course);
+            $scope.courseNow=courseEdit.resCourseSubject;
+        };
+    }).then(function(){
+        if($stateParams.subject){
+            $scope.changeSubject($stateParams.subject);
+            $scope.subjectNow=courseEdit.resSubject;
+        };
+    }).then(function(){
+        if($stateParams.subSubject){
+            $scope.changeSubSubject($stateParams.subSubject);
+            $scope.subSubjectNow=courseEdit.resSubSubject;
+            $scope.modulesNow=courseEdit.modulesOfThisSubSubject;
+        };
+    }).then(function(){
+        if($stateParams.module){
+            $scope.changeModule($stateParams.module);
+            $scope.moduleNow=courseEdit.moduleNow;
+        };
+    })
+
+
+    $scope.resCourseSubject={};
+    $scope.resCourseSubject.subjects=[];
+    $scope.resModuleLessonArray=[];
+    console.log(courseEdit);
+
+   // console.log(courseEdit.loadSubjects);
+   /* $scope.loadSubjects=courseEdit.loadSubjects;
+    $scope.resModuleLessonArray=courseEdit.resModuleLessonArray;*/
+
+
+  //  console.log($stateParams.subject)
+
+    $scope.changeCourse=function(name){
+        for(var i=0;i<$scope.loadSubjects.length;i++){
+            if($scope.loadSubjects[i].menuName===name){
+                $scope.resCourseSubject=$scope.loadSubjects[i];
+               // $scope.resModuleLessonArray=$scope.loadModule;
+                courseEdit.resCourseSubject=$scope.resCourseSubject;
+                courseEdit.resModuleLessonArray=$scope.resModuleLessonArray;
+               //$state.go('coursesedition');
+console.log(courseEdit.resSubject)
+                 if(courseEdit.resSubject){
+                     courseEdit.resSubject=null;
+                    // $scope.courseNow=courseEdit.resCourseSubject;
+                     $state.go('coursesedition');
+                 }
+console.log($scope.courseNow)
+            }
+        }
+    };
+    $scope.courseNow=courseEdit.resCourseSubject;
+
+    $scope.changeSubject=function(name){
+
+        for(var i=0;i<$scope.courseNow.subjects.length;i++){
+
+            if($scope.courseNow.subjects[i].subjectName===name){
+               $scope.resSubject=$scope.courseNow.subjects[i];
+                courseEdit.resSubject=$scope.resSubject;
+
+            }
+        }
+    }
+
+    $scope.subjectNow=courseEdit.resSubject;
+
+
+    $scope.changeSubSubject=function(name){
+
+        for(var i=0;i<$scope.subjectNow.subSubjects.length;i++){
+            if($scope.subjectNow.subSubjects[i].subSubjectName===name){
+                $scope.resSubSubject=$scope.subjectNow.subSubjects[i];
+                courseEdit.resSubSubject=$scope.resSubSubject;
+                //console.log( courseEdit.resSubject)
+                $scope.modulesOfThisSubSubject=[];
+
+               for(var j=0;j<$scope.loadModule.length;j++){
+
+                    if($scope.loadModule[j].parent===$scope.resSubSubject.specialId){
+
+                        $scope.modulesOfThisSubSubject.push($scope.loadModule[j]);
+                        courseEdit.modulesOfThisSubSubject=$scope.modulesOfThisSubSubject;
+                    }
+                }
+            }
+        }
+    }
+
+
+    $scope.subSubjectNow=courseEdit.resSubSubject;
+    $scope.modulesNow=courseEdit.modulesOfThisSubSubject;
+console.log($scope.modulesNow)
+
+
+
+ $scope.changeModule=function(name){
+
+     for(var i=0;i<$scope.modulesNow.length;i++){
+
+         if($scope.modulesNow[i].moduleName===name){
+             $scope.resModule=$scope.modulesNow[i];
+             courseEdit.moduleNow= $scope.resModule;
+             console.log(courseEdit.moduleNow)
+         }
+     }
+ }
+
+    $scope.moduleNow=courseEdit.moduleNow;
+
+
+    $scope.changeStep=function(name){
+
+        for(var i=0;i<$scope.moduleNow.steps.length;i++){
+
+            if($scope.moduleNow.steps[i].stepName===name){
+                $scope.resStep=$scope.moduleNow.steps[i];
+                courseEdit.stepNow= $scope.resStep;
+
+            }
+        }
+    }
+
+    $scope.stepNow=courseEdit.stepNow;
+
+$scope.obj=function(){
+    console.log($scope.courseNow,$scope.modulesNow)
+};
+
+
+
+    $scope.saveCourse=function(){
+
+        var data=[{action:'edit'},$scope.courseNow,$scope.moduleNow];
+        console.log(data);
+    $http.post('/subject',data).success(function (data) {
+            console.log(data);
+          /*  $scope.resCourseSubject._id=data.data.id;
+            $scope.getInventory();*/
+       });
+    };
+
+
+
+
+
+    $scope.status = {
+        isopen: false
+    };
+
+    $scope.toggled = function(open) {
+        console.log('Dropdown is now: ', open);
+    };
+
+    $scope.toggleDropdown = function($event) {
+        $event.preventDefault();
+        $event.stopPropagation();
+        $scope.status.isopen = !$scope.status.isopen;
+    };
+});
---
+++ public/js/controllers/lessonController.js
@@ -0,0 +1,203 @@
+/**
+ * Created by igor on 7/15/14.
+ */
+app.controller('lessonController', function ($scope, $http,$stateParams) {
+
+var init=function() {
+    $http.get('/courses').success(function (courses) {
+        $scope.courses = courses;
+//console.log( $scope.courses);
+        for (var i = 0; i < $scope.courses.length; i++) {
+            if ($scope.courses[i].title === $stateParams.courseTitle) {
+                $scope.course = $scope.courses[i];
+            }
+        }
+    });
+    $http.get('/units').success(function (units) {
+        $scope.units = units;
+
+        $scope.showUnits = function (id) {
+            var arrUnits = [];
+            for (var i = 0; i < $scope.units.length; i++) {
+                if ($scope.units[i].parent === id) {
+                    arrUnits.push($scope.units[i]);
+                }
+                ;
+            }
+            ;
+            return arrUnits;
+        };
+
+
+        console.log($scope.units)
+    });
+
+};
+    init();
+    $scope.unitsNew=[];
+
+
+
+    $scope.saveCourse=function(){
+        data=[{action:'edit'},$scope.course,$scope.units,$scope.unitsDelete,$scope.unitsNew];
+      console.log($scope.units)
+      $http.post('/subjects', data).success(function (data) {
+          // console.log(data.data.id);
+           // $scope.resCourseSubject._id=data.data.id;
+            //$scope.getInventory();
+          $scope.unitsNew=null;
+           console.log(data)
+         init()
+       });
+    };
+
+    $scope.deleteUnit=function(parent,unitTitle){
+
+        var unitsDelete=[];
+        var unitTitle=unitTitle||"all";
+        if(unitTitle==="all"){
+            for(var i=0;i<$scope.units.length;i++){
+                if($scope.units[i].parent===parent){
+                    unitsDelete.push($scope.units[i]._id);
+                    $scope.unitsDelete=unitsDelete;
+                    $scope.units.splice(i,1);
+                };
+            };
+            return
+        }
+        for(var i=0;i<$scope.units.length;i++){
+            if($scope.units[i].parent===parent&&$scope.units[i].title===unitTitle){
+                unitsDelete.push($scope.units[i]._id);
+                $scope.unitsDelete=unitsDelete;
+                $scope.units.splice(i,1);
+            };
+        };
+    };
+
+
+
+    $scope.deleteModule=function(id){
+        console.log($scope.course.modules)
+        for(var i=0;i<$scope.course.modules.length;i++){
+            if($scope.course.modules[i]._id===id){
+                for(var j=0;j<$scope.course.modules[i].sections.length;j++){
+                    $scope.deleteUnit($scope.course.modules[i].sections[j].specialId);
+                }
+                $scope.course.modules.splice(i,1);
+            }
+        }
+
+
+        console.log($scope.course.modules)
+    }
+
+
+    $scope.deleteSection=function(moduleTitle,sectionTitle){
+        for(var i=0;i<$scope.course.modules.length;i++){
+            if($scope.course.modules[i].title===moduleTitle){
+                for(var j= 0,l=$scope.course.modules[i].sections.length;j<l;j++){
+                    if($scope.course.modules[i].sections[j].title===sectionTitle){
+                        $scope.deleteUnit($scope.course.modules[i].sections[j].specialId)
+                        $scope.course.modules[i].sections.splice(j,1);
+                        j++;
+                    }
+
+                }
+                //$scope.course.modules.splice(i,1);
+            }
+        }
+    };
+
+
+
+
+    $scope.addModule=function(name,position){
+
+        for(var i= 0,l=$scope.course.modules.length;i<l;i++){
+            //console.log($scope.course.modules[i].title===name)
+           if($scope.course.modules[i].title===name){
+                console.log(position==='after')
+               if(position==='after'){
+                   //console.log(l)
+                   var specialId=Date.now();
+                    $scope.course.modules.splice(i+1,0,{title:"New Module",description:"",sections:[{title:"New Section",description:"",specialId:specialId}]})
+                   // console.log( $scope.course.modules);
+                   $scope.units.push({parent:specialId,title:"New Unit",description:"",lims:[]});
+                   $scope.saveCourse();
+                   return;
+                }
+               if(position==='before'){
+                  // console.log(l)
+                   var specialId=Date.now();
+                 $scope.course.modules.splice(i,0,{title:"New Module",description:"",sections:[{title:"New Section",description:"",specialId:specialId}]})
+                  // console.log( $scope.course.modules);
+                   $scope.units.push({parent:specialId,title:"New Unit",description:"",lims:[]});
+                   $scope.saveCourse();
+                   return;
+               }
+            }
+        }
+    }
+
+
+    $scope.addSection=function(name,position){
+        for(var i= 0;i<$scope.course.modules.length;i++){
+              for(var j= 0,l=$scope.course.modules[i].sections.length;j<l;j++){
+                if($scope.course.modules[i].sections[j].title===name){
+                    if(position==='after'){
+                        var specialId=Date.now();
+                        $scope.course.modules[i].sections.splice(j+1,0,{title:"New Section",description:"",specialId:specialId});
+                        // console.log( $scope.course.modules);
+                        $scope.units.push({parent:specialId,title:"New Unit",description:"",lims:[]});
+                        $scope.saveCourse();
+                        return;
+                    }
+                    if(position==='before'){
+                        var specialId=Date.now();
+                        $scope.course.modules[i].sections.splice(j,0,{title:"New Section",description:"",specialId:specialId});
+                        console.log( $scope.course.modules[i].sections);
+                        $scope.units.push({parent:specialId,title:"New Unit",description:"",lims:[]});
+                      //  $scope.unitsNew.push
+                        console.log($scope.units)
+                        $scope.saveCourse();
+                        return;
+                    }
+                }
+            }
+        }
+    }
+
+    $scope.addUnit=function(sectionTitle,unitsTitle,position){
+        for(var i= 0;i<$scope.course.modules.length;i++){
+            for(var j= 0,l=$scope.course.modules[i].sections.length;j<l;j++){
+                if($scope.course.modules[i].sections[j].title===sectionTitle){
+                    var specialId=$scope.course.modules[i].sections[j].specialId;
+                  //  for(var l=0;l< $scope.units)
+                    if(position==='after'){
+
+                        //$scope.course.modules[i].sections[j].splice(j+1,0,{title:"New Section",description:"",specialId:specialId});
+                        // console.log( $scope.course.modules);
+                        $scope.units.push(l-1,0,{parent:specialId,title:"New Unit",description:"",lims:[]});
+                       // $scope.saveCourse();
+                        return;
+                    }
+                    if(position==='before'){
+
+                       // $scope.course.modules[i].sections.splice(j,0,{title:"New Section",description:"",specialId:specialId});
+                        console.log( $scope.course.modules[i].sections);
+                        $scope.units.splice(0,1,{parent:specialId,title:"New Unit",description:"",lims:[]});
+                        //  $scope.unitsNew.push
+                        console.log($scope.units);
+                       // $scope.saveCourse();
+                        return;
+                    }
+                }
+            }
+        }
+
+    }
+
+
+
+
+})
